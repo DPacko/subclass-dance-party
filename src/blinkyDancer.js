a8e1fad281f9ce4a7266d0689d1b0a5799022571
DSaquin <dawnsaq@gmail.com>
2019-01-11 08:12:21 -0800

    added blinky

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -20,6 +20,17 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
+BlinkyDancer.prototype.step = function() {
+  var oldStep = Dancer.prototype.step;
+    // call the old version of step at the beginning of any call to this new version of step
+    oldStep.call(this);
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    this.$node.toggle();
+};
+
+
 BlinkyDancer.prototype.lineup = function() {
 
 }
